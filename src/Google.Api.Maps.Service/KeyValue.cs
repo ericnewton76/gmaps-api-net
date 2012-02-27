7efcedb814294ebdbbeb63c3610588211a456ee3
Eric Newton <ericnewton76@gmail.com>
2012-02-27 00:39:30 +0000

    these somehow didnt make it into merge, these files are from r18

---
+++ src/Google.Api.Maps.Service/KeyValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Google.Api.Maps.Service
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ValueText
+    {
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ src/Google.Api.Maps.Service/Waypoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Google.Api.Maps.Service
+{
+    public class Waypoint
+    {
+        public GeographicPosition Position { get; set; }
+        public string Address { get; set; }
+
+        public Waypoint(){}
+
+        public Waypoint(decimal lat, decimal lng)
+        {
+            Position = new GeographicPosition(lat, lng);
+        }
+
+        public override string ToString()
+        {
+            return Position != null ? Position.ToString() : Address;
+        }
+    }
+}
