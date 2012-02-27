d390422fb0f080bad1ee0b4fe051365a030d2a32
Eric Newton <ericnewton76@gmail.com>
2012-02-27 01:35:39 +0000

    bumped version numbers up to v0.11 in case prior version within repo was to be v0.10

--- src/Google.Maps/Properties/AssemblyInfo.cs
+++ src/Google.Maps/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Google Maps API for .NET Service")]
 [assembly: AssemblyDescription("Google Maps API for .NET service library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Luis Farzati")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Google Maps API for .NET")]
-[assembly: AssemblyCopyright("Copyright © Luis Farzati 2010")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2.*")]
+[assembly: AssemblyVersion("0.11.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
