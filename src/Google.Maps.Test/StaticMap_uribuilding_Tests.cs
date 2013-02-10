e212cf859c31dd522c042bf322645438671cb89e
Eric Newton <ericnewton76@gmail.com>
2013-02-10 20:07:48 +0000

    added file that was missing from project somehow

---
+++ src/Google.Maps.Test/StaticMap_uribuilding_Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Maps.StaticMaps;
+using Google.Maps;
+using NUnit.Framework;
+
+namespace Google.Maps.Test
+{
+    [TestFixture]
+    public class StaticMap_uribuilding_Tests
+    {
+        Uri gmapsBaseUri = new Uri("http://maps.google.com/");
+
+        [Test]
+        public void BasicUri()
+        {
+			StaticMapRequest sm = new StaticMapRequest()
+            {
+                Sensor = false,
+                Center = new LatLng(30.0, -90.0)
+            };
+
+            Uri actual = sm.ToUri();
+            Uri actualRelative = actual.MakeRelativeUri(gmapsBaseUri);
+
+			Uri expected = new Uri("/maps/api/json");
+
+			Assert.Inconclusive();
+        }
+    }
+}
