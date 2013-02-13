11858421e49a6053d0498115b4f80f76422ebe6e
Eric Newton <ericnewton76@gmail.com>
2013-02-13 00:34:43 -0500

    had to remove GetBoundsStr testing from GeocodingRequestTests.  Somewhere along the line this was removed.

--- src/Google.Maps.Test/GeocodingRequestTests.cs
+++ src/Google.Maps.Test/GeocodingRequestTests.cs
@@ -24,7 +24,13 @@ namespace Google.Maps.Test
             {
                 S_instanceType = typeof(GeocodingRequest);
                 _GetBoundsStr = S_instanceType.GetMethod("GetBoundsStr", BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Viewport) }, new ParameterModifier[] { });
+				Ensure(_GetBoundsStr, "GetBoundsStr");
             }
+
+			private static void Ensure(MethodInfo methodInfo, string methodName)
+			{
+				if (methodInfo == null) Assert.Fail("Method '{0}' on type '{1}' was not found, and the accessor will fail.", methodName, S_instanceType);
+			}
 
             public string GetBoundsStr(Viewport bounds)
             {
@@ -39,33 +45,48 @@ namespace Google.Maps.Test
             }
         }
 
-        [Test]
-        [ExpectedException(typeof(InvalidOperationException))]
-        public void Viewport_has_properties_notset()
-        {
-            Viewport bounds = new Viewport();
+		//[Test]
+		//[ExpectedException(typeof(InvalidOperationException))]
+		//public void Viewport_has_properties_notset()
+		//{
+		//    Viewport bounds = new Viewport();
 
-            GeocodingRequestAccessor request=new GeocodingRequestAccessor();
+		//    GeocodingRequestAccessor request=new GeocodingRequestAccessor();
 
-            string actual = request.GetBoundsStr(bounds);
-            string expected = "Expected an InvalidOperationException because viewport has a null northeast and southwest properties"; //expecting an exception
+		//    string actual = request.GetBoundsStr(bounds);
+		//    string expected = "Expected an InvalidOperationException because viewport has a null northeast and southwest properties"; //expecting an exception
 
-            Assert.Fail(expected);
-        }
+		//    Assert.Fail(expected);
+		//}
 
 
-        [Test]
-        public void GetBoundsStr()
-        {
-            Viewport bounds = new Viewport() { Southwest = new LatLng(30.0, -40.0), Northeast = new LatLng(40.0, -30.0) };
+		//[Test]
+		//public void GetBoundsStr()
+		//{
+		//    Viewport bounds = new Viewport() { Southwest = new LatLng(30.0, -40.0), Northeast = new LatLng(40.0, -30.0) };
 
-            GeocodingRequestAccessor request = new GeocodingRequestAccessor();
+		//    GeocodingRequestAccessor request = new GeocodingRequestAccessor();
 
-            string actual = request.GetBoundsStr(bounds);
-            string expected = "30.000000,-40.000000%7C40.000000,-30.000000";
+		//    string actual = request.GetBoundsStr(bounds);
+		//    string expected = "30.000000,-40.000000%7C40.000000,-30.000000";
 
-            Assert.AreEqual(expected, actual);
-        }
+		//    Assert.AreEqual(expected, actual);
+		//}
+
+		//[Test]
+		//[NUnit.Framework.SetCulture("ar-MA")]//set to arabic because of the multitude of cultural format changes
+		//public void GetBoundsStr_uses_invariant()
+		//{
+		//    Viewport bounds = new Viewport() { Southwest = new LatLng(30.0, -40.0), Northeast = new LatLng(40.0, -30.0) };
+
+		//    GeocodingRequestAccessor request = new GeocodingRequestAccessor();
+
+		//    string actual = request.GetBoundsStr(bounds);
+		//    string expected = "30.000000,-40.000000%7C40.000000,-30.000000";
+
+		//    Assert.AreEqual(expected, actual);
+		//}
+
 
     }
 }
