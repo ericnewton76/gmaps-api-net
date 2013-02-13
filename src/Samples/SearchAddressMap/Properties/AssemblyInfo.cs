757328e67a3bfaf5dba56485ebb33ce4d3bc0196
Eric Newton <ericnewton76@gmail.com>
2013-02-13 02:57:40 -0500

    Version bump to 0.10

--- src/Samples/SearchAddressMap/Properties/AssemblyInfo.cs
+++ src/Samples/SearchAddressMap/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.4.15812")]
+// [assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.15813")]
