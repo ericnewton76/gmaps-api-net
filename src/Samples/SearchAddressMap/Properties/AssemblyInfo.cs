5dbd1f6e174c320af509702f19493b7990981efe
Eric Newton <ericnewton76@gmail.com>
2013-02-12 23:57:16 -0500

    Updated version to 0.9.4

--- src/Samples/SearchAddressMap/Properties/AssemblyInfo.cs
+++ src/Samples/SearchAddressMap/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.4.15812")]
