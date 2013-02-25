a04414ee7001d65ecc7812d439d768f14bd6448d
Eric Newton <ericnewton76@gmail.com>
2013-02-25 17:52:18 -0500

    added summaries to most of the public properties.  Note that this indicates Bounds doesnt exist for the Directions API and a few other changes are coming.

--- src/Google.Maps/Direction/DirectionRequest.cs
+++ src/Google.Maps/Direction/DirectionRequest.cs
@@ -2,66 +2,88 @@
 using System.Collections.Generic;
 using System.Text;
 using Google.Maps;
+using System.ComponentModel;
 
 namespace Google.Maps.Direction
 {
-    public class DirectionRequest
-    {
-        public Waypoint Origin { get; set; }
-        public Waypoint Destination { get; set; }
+	public class DirectionRequest
+	{
+		/// <summary>
+		/// The <see cref="Location"/> from which you wish to calculate directions.
+		/// </summary>
+		public Waypoint Origin { get; set; }
+		/// <summary>
+		/// The <see cref="Location"/> from which you wish to calculate directions.
+		/// </summary>
+		public Waypoint Destination { get; set; }
 
-        public TravelMode Mode { get; set; }
+		/// <summary>
+		/// Specifies the mode of transport to use when calculating directions. Valid values are specified in <see cref="TravelMode"/>s. 
+		/// </summary>
+		//TODO: add transit TravelMode and add to summary: If you set the mode to "transit" you must also specify either a departure_time or an arrival_time.
+		[DefaultValue(TravelMode.driving)]
+		public TravelMode Mode { get; set; }
 
-        /// <see cref="http://code.google.com/apis/maps/documentation/directions/#RequestParameters"/>
-        public string Bounds { get; set; }
+		/// <summary>Unsupported, removing in a future commit.</summary>
+		/// <see cref="http://code.google.com/apis/maps/documentation/directions/#RequestParameters"/>
+		// TODO: REMOVE the Bounds property. No mention in the request api for directions.
+		public string Bounds { get; set; }
 
-        /// <see cref="http://code.google.com/apis/maps/documentation/directions/#RequestParameters"/>
-        public string Region { get; set; }
+		/// <summary>The region code, specified as a ccTLD ("top-level domain") two-character value. See also Region biasing.</summary>
+		/// <see cref="http://code.google.com/apis/maps/documentation/directions/#RequestParameters"/>
+		/// <seealso cref="https://developers.google.com/maps/documentation/directions/#RegionBiasing"/>
+		public string Region { get; set; }
 
-        /// <see cref="http://code.google.com/apis/maps/documentation/directions/#RequestParameters"/>
-        public string Language { get; set; }
+		/// <summary>The language in which to return results. See the list of supported domain languages. 
+		/// Note that we often update supported languages so this list may not be exhaustive. 
+		/// If language is not supplied, the service will attempt to use the native language of the domain from which the request is sent.</summary>
+		/// <see cref="http://code.google.com/apis/maps/documentation/directions/#RequestParameters"/>
+		public string Language { get; set; }
 
-        public bool Sensor { get; set; }
+		/// <summary>
+		///  Indicates whether or not the directions request comes from a device with a location sensor. This value must be either true or false.
+		/// </summary>
+		public bool Sensor { get; set; }
 
-        private SortedList<int, Waypoint> waypoints;
-        public SortedList<int, Waypoint> Waypoints
-        {
-            get
-            {
-                if (waypoints == null)
-                {
-                    waypoints = new SortedList<int, Waypoint>();
-                }
-                return waypoints;
-            }
-            set
-            {
-                waypoints = value;
-            }
-        }
+		private SortedList<int, Waypoint> waypoints;
+		public SortedList<int, Waypoint> Waypoints
+		{
+			get
+			{
+				if (waypoints == null)
+				{
+					waypoints = new SortedList<int, Waypoint>();
+				}
+				return waypoints;
+			}
+			set
+			{
+				waypoints = value;
+			}
+		}
 
-        public void Add(Waypoint location)
-        {
-            Waypoints.Add(Waypoints.Count, location);
-        }
+		public void Add(Waypoint location)
+		{
+			Waypoints.Add(Waypoints.Count, location);
+		}
 
-        internal string WaypointsToUri()
-        {
-            if (Waypoints.Count == 0) return string.Empty;
+		internal string WaypointsToUri()
+		{
+			if (Waypoints.Count == 0) return string.Empty;
 
-            StringBuilder sb = new StringBuilder();
+			StringBuilder sb = new StringBuilder();
 
-            foreach (Waypoint waypoint in Waypoints.Values)
-            {
-                sb.AppendFormat("{0}|", waypoint.ToString());
-            }
-            sb = sb.Remove(sb.Length - 1, 1);
+			foreach (Waypoint waypoint in Waypoints.Values)
+			{
+				sb.AppendFormat("{0}|", waypoint.ToString());
+			}
+			sb = sb.Remove(sb.Length - 1, 1);
 
-            return sb.ToString();
-        }
+			return sb.ToString();
+		}
 
-        internal Uri ToUri()
-        {
+		internal Uri ToUri()
+		{
 			var qsb = new Google.Maps.Internal.QueryStringBuilder()
 				.Append("origin=", Origin.ToString())
 				.Append("destination=", Destination.ToString())
@@ -73,7 +95,7 @@ namespace Google.Maps.Direction
 
 			var url = "json?" + qsb.ToString();
 
-            return new Uri(url, UriKind.Relative);
-        }
-    }
+			return new Uri(url, UriKind.Relative);
+		}
+	}
 }
