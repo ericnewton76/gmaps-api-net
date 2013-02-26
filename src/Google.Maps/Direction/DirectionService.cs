32f8774388d550af36ce32268097e4fe43c92540
Eric Newton <ericnewton76@gmail.com>
2013-02-25 20:20:44 -0500

    DirectionService now uses HTTPS by default, and can use HTTP if desired.

--- src/Google.Maps/Direction/DirectionService.cs
+++ src/Google.Maps/Direction/DirectionService.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
+
 namespace Google.Maps.Direction
 {
 	public class DirectionService
 	{
-		public static readonly Uri ApiUrl =
+		public static readonly Uri HttpUri =
 			new Uri("http://maps.googleapis.com/maps/api/directions/");
+		public static readonly Uri HttpsUri =
+			new Uri("https://maps.googleapis.com/maps/api/directions/");
+
+		public DirectionService() : this(HttpsUri)
+		{
+		}
+		public DirectionService(Uri baseUri)
+		{
+			this.BaseUri = HttpsUri;
+		}
+
+		public Uri BaseUri { get; set; }
 
 		public DirectionResponse GetResponse(DirectionRequest request)
 		{
-			var url = new Uri(ApiUrl, request.ToUri());
+			var url = new Uri(this.BaseUri, request.ToUri());
 			return Internal.Http.Get(url).As<DirectionResponse>();
 		}
 	}
