314070b00b3cb95f4a223fa4af5d3ec394125cb7
Eric Newton <ericnewton76@gmail.com>
2013-02-26 00:09:43 -0500

    added summary info to DistanceMatrixRequest and StaticMapRequest

--- src/Google.Maps/DistanceMatrix/DistanceMatrixService.cs
+++ src/Google.Maps/DistanceMatrix/DistanceMatrixService.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Google.Maps.DistanceMatrix
 {
+	/// <summary>
+	/// The Google Distance Matrix API is a service that provides travel distance and time for a matrix of origins and destinations. The information returned is based on the recommended route 
+	/// between start and end points, as calculated by the Google Maps API, and consists of rows containing duration and distance values for each pair.
+	/// <para>This service does not return detailed route information. Route information can be obtained by passing the desired single origin and destination to the Directions API.</para>
+	/// </summary>
+	/// <see cref="http://developers.google.com/maps/documentation/distancematrix/"/>
 	public static class DistanceMatrixService
 	{
 
