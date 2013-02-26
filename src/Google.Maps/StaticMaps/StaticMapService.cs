0ca93ba914c62e2f5fda7f1d6d88f01bdb433938
Eric Newton <ericnewton76@gmail.com>
2013-02-25 23:37:25 -0500

    added summary description to DistanceMatrixService and StaticMapService

--- src/Google.Maps/StaticMaps/StaticMapService.cs
+++ src/Google.Maps/StaticMaps/StaticMapService.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
@@ -8,6 +24,12 @@ using Size = System.Drawing.Size;
 
 namespace Google.Maps.StaticMaps
 {
+	/// <summary>
+	/// The Google Static Maps API lets you embed a Google Maps image on your web page without requiring JavaScript or any dynamic page 
+	/// loading. The Google Static Map service creates your map based on URL parameters sent through a standard HTTP request and returns the 
+	/// map as an image you can display on your web page.
+	/// </summary>
+	/// <see cref="https://developers.google.com/maps/documentation/staticmaps/"/>
     public class StaticMapService
     {
         public StaticMapService()
