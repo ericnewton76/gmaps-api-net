8839f9016dd6879d93696b4027c978bdbe4a7b13
Eric Newton <ericnewton76@gmail.com>
2013-02-26 03:05:01 -0500

    Attempted to rectify tests failing due to latlng conversion problems but ran into Elevation precision tolerance being ignored somehow.

--- src/Google.Maps.Test/Elevation/ElevationServiceTests.cs
+++ src/Google.Maps.Test/Elevation/ElevationServiceTests.cs
@@ -42,9 +42,8 @@ namespace Google.Maps.Test.Elevation
 			// expectations
 			var expectedStatus = ServiceResponseStatus.Ok;
 			var expectedResultCount = 1;
-			var expectedElevation = 1608.8402100m;
-			var expectedLocationLatitude = 39.7391536m;
-			var expectedLocationLongitude = -104.9847034m;
+			var expectedElevation = 1608.6m;
+			var expectedLocation = new LatLng(39.739153, -104.984703);
 			
 			// test
 			var request = new ElevationRequest();
@@ -55,9 +54,10 @@ namespace Google.Maps.Test.Elevation
 			// asserts
 			Assert.AreEqual(expectedStatus, response.Status);
 			Assert.AreEqual(expectedResultCount, response.Results.Length);
-			Assert.AreEqual(expectedElevation, response.Results.Single().Elevation);
-			Assert.AreEqual(expectedLocationLatitude, response.Results.Single().Location.Latitude);
-			Assert.AreEqual(expectedLocationLongitude, response.Results.Single().Location.Longitude);
+
+			Assert.That(expectedElevation, Is.EqualTo(response.Results[0].Elevation).Within(0.09), "Result.Elevation");
+
+			Assert.That(expectedLocation, Is.EqualTo(response.Results[0].Location).Using(LatLngComparer.Within(0.000001f)), "Result.Location");
 		}
 
 		[Test]
@@ -66,12 +66,10 @@ namespace Google.Maps.Test.Elevation
 			// expectations
 			var expectedStatus = ServiceResponseStatus.Ok;
 			var expectedResultCount = 2;
-			var expectedElevation1 = 1608.8402100m;
-			var expectedLocationLatitude1 = 39.7391536m;
-			var expectedLocationLongitude1 = -104.9847034m;
-			var expectedElevation2 = -50.7890358m;
-			var expectedLocationLatitude2 = 36.4555560m;
-			var expectedLocationLongitude2 = -116.8666670m;
+			var expectedElevation1 = 1608.6m;
+			var expectedLocation1 = new LatLng(39.739153,-104.984703);
+			var expectedElevation2 = -50.8m;
+			var expectedLocation2 = new LatLng(36.4555560, -116.866667);
 
 			// test
 			var request = new ElevationRequest();
@@ -81,13 +79,14 @@ namespace Google.Maps.Test.Elevation
 
 			// asserts
 			Assert.AreEqual(expectedStatus, response.Status);
-			Assert.AreEqual(expectedResultCount, response.Results.Length);
-			Assert.AreEqual(expectedElevation1, response.Results.First().Elevation);
-			Assert.AreEqual(expectedLocationLatitude1, response.Results.First().Location.Latitude);
-			Assert.AreEqual(expectedLocationLongitude1, response.Results.First().Location.Longitude);
-			Assert.AreEqual(expectedElevation2, response.Results.Last().Elevation);
-			Assert.AreEqual(expectedLocationLatitude2, response.Results.Last().Location.Latitude);
-			Assert.AreEqual(expectedLocationLongitude2, response.Results.Last().Location.Longitude);
+			Assert.AreEqual(expectedResultCount, response.Results.Length, "Results.Length");
+
+			Assert.That(expectedElevation1, Is.EqualTo(response.Results[0].Elevation).Within(0.09), "Results[0].Elevation");
+			Assert.That(expectedLocation1, Is.EqualTo(response.Results[0].Location).Using(LatLngComparer.Within(0.000001f)), "Results[0].Location");
+
+			Assert.AreEqual(expectedElevation2, Is.EqualTo(response.Results[1].Elevation).Within(0.09), "Results[1].Elevation");
+			Assert.AreEqual(expectedLocation2, Is.EqualTo(response.Results[1].Location).Using(LatLngComparer.Within(0.000001f)), "Results[1].Location");
+
 		}
 	}
 }
