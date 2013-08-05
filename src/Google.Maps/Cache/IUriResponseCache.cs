4d40b72f8acb635088de6317761a25f191276768
P. Büttiker <isnull@securityvision.ch>
2013-08-05 11:04:24 +0200

    basic Uri response cache implementation

---
+++ src/Google.Maps/Cache/IUriResponseCache.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Google.Maps.Cache
+{
+    /// <summary>
+    /// Provides a generic Uri-request response cache
+    /// </summary>
+    public interface IUriResponseCache
+    {
+        /// <summary>
+        /// Cache lookup for the given Uri, if avaiable, returns the cached response
+        /// </summary>
+        /// <param name="request">The request Uri</param>
+        /// <param name="cacheLifetime">The maximum age of a cached item. 
+        /// (If the cached item is older, null is returned)</param>
+        /// <returns>Returns the cached data or null when no cached item is avaiable</returns>
+        string GetContent(Uri request, TimeSpan cacheLifetime);
+
+        /// <summary>
+        /// Store the given Uri / response in the cache
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="responseContent"></param>
+        void SaveContent(Uri request, string responseContent);
+    }
+}
--- src/Google.Maps/Internal/Http.cs
+++ src/Google.Maps/Internal/Http.cs
@@ -18,6 +18,8 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Text;
+using Google.Maps.Cache;
 using Newtonsoft.Json;
 
 namespace Google.Maps.Internal
@@ -27,61 +29,163 @@ namespace Google.Maps.Internal
 	/// </summary>
 	public static class Http
 	{
+	    /// <summary>
+	    /// How old can a cached response be at max?
+	    /// </summary>
+	    public static TimeSpan DefaultCacheLifeTime = TimeSpan.FromDays(1);
+
+
+        /// <summary>
+        /// Represents a http response
+        /// </summary>
 		public class HttpGetResponse
-		{
+        {
+
+            private readonly IUriResponseCache _uriResponseCache;
+
+            /// <summary>
+            /// The request uri
+            /// </summary>
 			protected Uri RequestUri { get; set; }
 
-			public HttpGetResponse(Uri uri)
+            /// <summary>
+            /// Shall this request use the uri cache?
+            /// </summary>
+            public bool UseCache
+            {
+                get { return _uriResponseCache != null; }
+            }
+
+            /// <summary>
+            /// Was the data of this response fetched from the cache?
+            /// </summary>
+            public bool FromCache { get; private set; }
+
+
+            /// <summary>
+            /// Creates a new HttpResponse with the given uri
+            /// </summary>
+            /// <param name="uri"></param>
+            public HttpGetResponse(Uri uri)
+                :this(uri, null)
+            {
+            }
+
+            /// <summary>
+            /// Creates a new HttpResponse with the given uri and the given response cache
+            /// </summary>
+            /// <param name="uri"></param>
+            /// <param name="uriResponseCache"></param>
+            public HttpGetResponse(Uri uri, IUriResponseCache uriResponseCache)
 			{
 				RequestUri = uri;
+                _uriResponseCache = uriResponseCache;
 			}
 
 			protected virtual StreamReader GetStreamReader(Uri uri)
 			{
 				return GetStreamReader(uri, GoogleSigned.SigningInstance);
 			}
-			protected virtual StreamReader GetStreamReader(Uri uri, GoogleSigned signingInstance)
-			{
-				if (signingInstance != null)
-				{
-					uri = new Uri(signingInstance.GetSignedUri(uri));
-				}
 
-				WebResponse response = WebRequest.Create(uri).GetResponse();
 
-				StreamReader sr = new StreamReader(response.GetResponseStream());
-				return sr;
-			}
+            protected virtual StreamReader GetStreamReader(Uri uri, GoogleSigned signingInstance)
+            {
+                StreamReader sr = null;
 
+                var requestUri = uri;
+                if (signingInstance != null)
+                {
+                    requestUri = new Uri(signingInstance.GetSignedUri(uri));
+                }
+
+                var response = WebRequest.Create(requestUri).GetResponse();
+                var rs = response.GetResponseStream();
+                if (rs != null)
+                    sr = new StreamReader(rs);
+
+                return sr;
+            }
+
+            /// <summary>
+            /// Returns the cached response for the given Uri if avaiable
+            /// </summary>
+            /// <param name="uri"></param>
+            /// <param name="cacheLifetime"></param>
+            /// <returns></returns>
+            private string GetCachedResponse(Uri uri, TimeSpan cacheLifetime)
+            {
+                string cachedData = null;
+                if(_uriResponseCache != null)
+                {
+                    cachedData = _uriResponseCache.GetContent(uri, cacheLifetime);
+                }
+                return cachedData;
+            }
+
+
+            private void SaveToCache(Uri uri, string data)
+            {
+                if (_uriResponseCache != null)
+                {
+                    _uriResponseCache.SaveContent(uri, data);
+                }
+            }
+
+
+
+            /// <summary>
+            /// Returns the response data as string
+            /// </summary>
+            /// <returns></returns>
 			public virtual string AsString()
 			{
 				var output = String.Empty;
 
-				using (var reader = GetStreamReader(this.RequestUri))
-				{
-					output = reader.ReadToEnd();
-				}
+                if (UseCache)
+                    output = GetCachedResponse(RequestUri, DefaultCacheLifeTime);
 
-				return output;
+                if (String.IsNullOrEmpty(output))
+                {
+                    using (var reader = GetStreamReader(this.RequestUri))
+                    {
+                        output = reader.ReadToEnd();
+                        SaveToCache(RequestUri, output);
+                    }
+                }
+
+                return output;
 			}
 
-			public virtual T As<T>() where T : class
+			/// <summary>
+			/// Parses the response into its JSON object representation
+			/// </summary>
+			/// <typeparam name="T"></typeparam>
+			/// <returns></returns>
+            public virtual T As<T>() where T : class
 			{
-				T output = null;
+			    T output = null;
 
-				using (var reader = GetStreamReader(this.RequestUri))
-				{
-					JsonTextReader jsonReader = new JsonTextReader(reader);
-					JsonSerializer serializer = new JsonSerializer();
-					serializer.Converters.Add(new JsonEnumTypeConverter());
-					serializer.Converters.Add(new JsonLocationConverter());
-					output = serializer.Deserialize<T>(jsonReader);
-				}
+			    var responseData = AsString();
 
-				return output;
+                using (var sr = new StringReader(responseData))
+                using (var jsonReader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer();
+                    serializer.Converters.Add(new JsonEnumTypeConverter());
+                    serializer.Converters.Add(new JsonLocationConverter());
+
+                    output = serializer.Deserialize<T>(jsonReader);
+                }
+
+			    return output;
 			}
-		}
+        }
 
+		/// <summary>
+		/// Get the 
+		/// </summary>
+		/// <param name="uri"></param>
+		/// <returns></returns>
 		public static HttpGetResponse Get(Uri uri)
 		{
 			return Factory.CreateResponse(uri);
@@ -92,11 +196,18 @@ namespace Google.Maps.Internal
 		/// </summary>
 		public static HttpGetResponseFactory Factory = new HttpGetResponseFactory();
 
+
 		/// <summary>
 		/// A factory class for building HttpGetResponse instances.
 		/// </summary>
 		public class HttpGetResponseFactory
 		{
+            /// <summary>
+            /// Gets/Sets the UriResponse cache
+            /// </summary>
+		    public IUriResponseCache UriResponseCache { get; set; }
+		
+
 			/// <summary>
 			/// Builds a standard HttpGetResponse instance.
 			/// </summary>
@@ -104,7 +215,7 @@ namespace Google.Maps.Internal
 			/// <returns></returns>
 			public virtual HttpGetResponse CreateResponse(Uri uri)
 			{
-				return new HttpGetResponse(uri);
+                return new HttpGetResponse(uri, UriResponseCache);
 			}
 		}
 
