35802c199bd0bf2bd486b15d35a8fb89718c3008
Danila Polevshikov <dpo@digt.com>
2014-02-28 13:15:00 +0400

    Add optional proxy for requests. Introduce Proxy.AssignAllServices()

--- src/Google.Maps/Internal/Http.cs
+++ src/Google.Maps/Internal/Http.cs
@@ -38,17 +38,23 @@ namespace Google.Maps.Internal
 
 			protected virtual StreamReader GetStreamReader(Uri uri)
 			{
-				return GetStreamReader(uri, GoogleSigned.SigningInstance);
+				return GetStreamReader(uri, GoogleSigned.SigningInstance, Proxy.ProxyInstance);
 			}
-			protected virtual StreamReader GetStreamReader(Uri uri, GoogleSigned signingInstance)
+			protected virtual StreamReader GetStreamReader(Uri uri, GoogleSigned signingInstance, IWebProxy proxyInstance)
 			{
 				if (signingInstance != null)
 				{
 					uri = new Uri(signingInstance.GetSignedUri(uri));
 				}
+				
+				WebRequest request = WebRequest.Create(uri);
+				
+				if (proxyInstance != null)
+				{
+					request.Proxy = proxyInstance;
+				}
 
-				WebResponse response = WebRequest.Create(uri).GetResponse();
-
+				WebResponse response = request.GetResponse();
 				StreamReader sr = new StreamReader(response.GetResponseStream());
 				return sr;
 			}
