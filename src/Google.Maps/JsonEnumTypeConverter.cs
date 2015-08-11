c110d9e59e9fa2b2bdf8fbd0c8709837b1eeb8be
Isaac Jordan <sheepses6@hotmail.co.uk>
2015-08-11 17:11:06 +0100

    Another attempt to restore the whitespace issue.

--- src/Google.Maps/JsonEnumTypeConverter.cs
+++ src/Google.Maps/JsonEnumTypeConverter.cs
@@ -22,142 +22,119 @@ using Newtonsoft.Json;
 namespace Google.Maps
 {
 
-    public class JsonEnumTypeConverter : JsonConverter
-    {
-        public static ServiceResponseStatus AsResponseStatus(string s)
-        {
-            var result = ServiceResponseStatus.Unknown;
+	public class JsonEnumTypeConverter : JsonConverter
+	{
+		public static ServiceResponseStatus AsResponseStatus(string s)
+		{
+			var result = ServiceResponseStatus.Unknown;
 
-            switch (s)
-            {
-                case "OK":
-                    result = ServiceResponseStatus.Ok;
-                    break;
-                case "ZERO_RESULTS":
-                    result = ServiceResponseStatus.ZeroResults;
-                    break;
-                case "OVER_QUERY_LIMIT":
-                    result = ServiceResponseStatus.OverQueryLimit;
-                    break;
-                case "REQUEST_DENIED":
-                    result = ServiceResponseStatus.RequestDenied;
-                    break;
-                case "INVALID_REQUEST":
-                    result = ServiceResponseStatus.InvalidRequest;
-                    break;
-            }
+			switch (s)
+			{
+				case "OK":
+					result = ServiceResponseStatus.Ok;
+					break;
+				case "ZERO_RESULTS":
+					result = ServiceResponseStatus.ZeroResults;
+					break;
+				case "OVER_QUERY_LIMIT":
+					result = ServiceResponseStatus.OverQueryLimit;
+					break;
+				case "REQUEST_DENIED":
+					result = ServiceResponseStatus.RequestDenied;
+					break;
+				case "INVALID_REQUEST":
+					result = ServiceResponseStatus.InvalidRequest;
+					break;
+			}
 
-            return result;
-        }
+			return result;
+		}
 
-        public static AddressType AsAddressType(string s)
-        {
-            var result = AddressType.Unknown;
+		public static AddressType AsAddressType(string s)
+		{
+			var result = AddressType.Unknown;
 
-            switch (s)
-            {
-                case "street_address":
-                    result = AddressType.StreetAddress;
-                    break;
-                case "route":
-                    result = AddressType.Route;
-                    break;
-                case "intersection":
-                    result = AddressType.Intersection;
-                    break;
-                case "political":
-                    result = AddressType.Political;
-                    break;
-                case "country":
-                    result = AddressType.Country;
-                    break;
-                case "administrative_area_level_1":
-                    result = AddressType.AdministrativeAreaLevel1;
-                    break;
-                case "administrative_area_level_2":
-                    result = AddressType.AdministrativeAreaLevel2;
-                    break;
-                case "administrative_area_level_3":
-                    result = AddressType.AdministrativeAreaLevel3;
-                    break;
-                case "colloquial_area":
-                    result = AddressType.ColloquialArea;
-                    break;
-                case "locality":
-                    result = AddressType.Locality;
-                    break;
-                case "sublocality":
-                    result = AddressType.Sublocality;
-                    break;
-                case "neighborhood":
-                    result = AddressType.Neighborhood;
-                    break;
-                case "premise":
-                    result = AddressType.Premise;
-                    break;
-                case "subpremise":
-                    result = AddressType.Subpremise;
-                    break;
-                case "postal_code":
-                    result = AddressType.PostalCode;
-                    break;
-                case "postal_town":
-                    result = AddressType.PostalTown;
-                    break;
-                case "postal_code_prefix":
-                    result = AddressType.PostalCodePrefix;
-                    break;
-                case "natural_feature":
-                    result = AddressType.NaturalFeature;
-                    break;
-                case "airport":
-                    result = AddressType.Airport;
-                    break;
-                case "park":
-                    result = AddressType.Park;
-                    break;
-                case "point_of_interest":
-                    result = AddressType.PointOfInterest;
-                    break;
-                case "post_box":
-                    result = AddressType.PostBox;
-                    break;
-                case "street_number":
-                    result = AddressType.StreetNumber;
-                    break;
-                case "floor":
-                    result = AddressType.Floor;
-                    break;
-                case "room":
-                    result = AddressType.Room;
-                    break;
-            }
+			switch (s)
+			{
+				case "street_address":
+					result = AddressType.StreetAddress;
+					break;
+				case "route":
+					result = AddressType.Route;
+					break;
+				case "intersection":
+					result = AddressType.Intersection;
+					break;
+				case "political":
+					result = AddressType.Political;
+					break;
+				case "country":
+					result = AddressType.Country;
+					break;
+				case "administrative_area_level_1":
+					result = AddressType.AdministrativeAreaLevel1;
+					break;
+				case "administrative_area_level_2":
+					result = AddressType.AdministrativeAreaLevel2;
+					break;
+				case "administrative_area_level_3":
+					result = AddressType.AdministrativeAreaLevel3;
+					break;
+				case "colloquial_area":
+					result = AddressType.ColloquialArea;
+					break;
+				case "locality":
+					result = AddressType.Locality;
+					break;
+				case "sublocality":
+					result = AddressType.Sublocality;
+					break;
+				case "neighborhood":
+					result = AddressType.Neighborhood;
+					break;
+				case "premise":
+					result = AddressType.Premise;
+					break;
+				case "subpremise":
+					result = AddressType.Subpremise;
+					break;
+				case "postal_code":
+					result = AddressType.PostalCode;
+					break;
+				case "postal_town":
+					result = AddressType.PostalTown;
+					break;
+				case "postal_code_prefix":
+					result = AddressType.PostalCodePrefix;
+					break;
+				case "natural_feature":
+					result = AddressType.NaturalFeature;
+					break;
+				case "airport":
+					result = AddressType.Airport;
+					break;
+				case "park":
+					result = AddressType.Park;
+					break;
+				case "point_of_interest":
+					result = AddressType.PointOfInterest;
+					break;
+				case "post_box":
+					result = AddressType.PostBox;
+					break;
+				case "street_number":
+					result = AddressType.StreetNumber;
+					break;
+				case "floor":
+					result = AddressType.Floor;
+					break;
+				case "room":
+					result = AddressType.Room;
+					break;
+			}
 
-            return result;
-        }
-
-        public static LocationType AsLocationType(string s)
-        {
-            var result = LocationType.Unknown;
-
-            switch (s)
-            {
-                case "ROOFTOP":
-                    result = LocationType.Rooftop;
-                    break;
-                case "RANGE_INTERPOLATED":
-                    result = LocationType.RangeInterpolated;
-                    break;
-                case "GEOMETRIC_CENTER":
-                    result = LocationType.GeometricCenter;
-                    break;
-                case "APPROXIMATE":
-                    result = LocationType.Approximate;
-                    break;
-            }
-
-            return result;
-        }
+			return result;
+		}
 
         private static Places.PlaceType AsPlaceType(string s)
         {
@@ -546,37 +523,60 @@ namespace Google.Maps
             return result;
         }
 
-        public override bool CanConvert(Type objectType)
-        {
-            return
-                objectType == typeof(ServiceResponseStatus)
-                || objectType == typeof(AddressType)
-                || objectType == typeof(LocationType)
+		public static LocationType AsLocationType(string s)
+		{
+			var result = LocationType.Unknown;
+
+			switch (s)
+			{
+				case "ROOFTOP":
+					result = LocationType.Rooftop;
+					break;
+				case "RANGE_INTERPOLATED":
+					result = LocationType.RangeInterpolated;
+					break;
+				case "GEOMETRIC_CENTER":
+					result = LocationType.GeometricCenter;
+					break;
+				case "APPROXIMATE":
+					result = LocationType.Approximate;
+					break;
+			}
+
+			return result;
+		}
+	
+		public override bool CanConvert(Type objectType)
+		{
+			return
+				objectType == typeof(ServiceResponseStatus)
+				|| objectType == typeof(AddressType)
+				|| objectType == typeof(LocationType)
                 || objectType == typeof(Places.PlaceType);
-        }
+		}
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            object result = null;
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			object result = null;
 
-            if (objectType == typeof(ServiceResponseStatus))
-                result = AsResponseStatus(reader.Value.ToString());
+			if(objectType == typeof(ServiceResponseStatus))
+				result = AsResponseStatus(reader.Value.ToString());
 
-            if (objectType == typeof(AddressType))
-                result = AsAddressType(reader.Value.ToString());
+			if (objectType == typeof(AddressType))
+				result = AsAddressType(reader.Value.ToString());
 
-            if (objectType == typeof(LocationType))
-                result = AsLocationType(reader.Value.ToString());
+			if (objectType == typeof(LocationType))
+				result = AsLocationType(reader.Value.ToString());
 
             if (objectType == typeof(Places.PlaceType))
                 result = AsPlaceType(reader.Value.ToString());
 
-            return result;
-        }
+			return result;
+		}
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
