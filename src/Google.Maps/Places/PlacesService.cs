7d07671b3d2dc599b70a8dd6c9b73f59b6cad739
Isaac Jordan <sheepses6@hotmail.co.uk>
2015-08-25 09:33:50 +0100

    Removed unnecessary line from PlacesService

--- src/Google.Maps/Places/PlacesService.cs
+++ src/Google.Maps/Places/PlacesService.cs
@@ -54,7 +54,6 @@ namespace Google.Maps.Places
 		public PlacesResponse GetResponse<TRequest>(TRequest request) where TRequest : PlacesRequest
 		{
 			var url = new Uri(this.BaseUri, request.ToUri());
-			var t = Internal.Http.Get(url).AsString();
 			return Internal.Http.Get(url).As<PlacesResponse>();
 		}
 
