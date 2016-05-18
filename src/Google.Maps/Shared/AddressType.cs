2341929b6533129c7d7e2247f01ae093df5f78bd
Alexandre Jobin <alexandre.jobin@tourisme.gouv.qc.ca>
2016-05-18 13:23:00 -0400

    Added more address types

--- src/Google.Maps/Shared/AddressType.cs
+++ src/Google.Maps/Shared/AddressType.cs
@@ -17,147 +17,186 @@
 
 namespace Google.Maps.Shared
 {
-	public enum AddressType
-	{
-		Unknown = 0,
+    public enum AddressType
+    {
+        Unknown = 0,
 
-		/// <summary>
-		/// Indicates a precise street address.
-		/// </summary>
-		StreetAddress = 1,
+        /// <summary>
+        /// Indicates a precise street address.
+        /// </summary>
+        StreetAddress = 1,
 
-		/// <summary>
-		/// Indicates a named route (such as "US 101").
-		/// </summary>
-		Route = 2,
+        /// <summary>
+        /// Indicates a named route (such as "US 101").
+        /// </summary>
+        Route = 2,
 
-		/// <summary>
-		/// Indicates a major intersection, usually of two major roads.
-		/// </summary>
-		Intersection = 3,
+        /// <summary>
+        /// Indicates a major intersection, usually of two major roads.
+        /// </summary>
+        Intersection = 3,
 
-		/// <summary>
-		/// Indicates a political entity. Usually, this type indicates a polygon of
-		/// some civil administration.
-		/// </summary>
-		Political = 4,
+        /// <summary>
+        /// Indicates a political entity. Usually, this type indicates a polygon of
+        /// some civil administration.
+        /// </summary>
+        Political = 4,
 
-		/// <summary>
-		/// Indicates the national political entity, and is typically the highest
-		/// order type returned by the Geocoder.
-		/// </summary>
-		Country = 5,
+        /// <summary>
+        /// Indicates the national political entity, and is typically the highest
+        /// order type returned by the Geocoder.
+        /// </summary>
+        Country = 5,
 
-		/// <summary>
-		/// Indicates a first-order civil entity below the country level. Within the
-		/// United States, these administrative levels are states. Not all nations
-		/// exhibit these administrative levels.
-		/// </summary>
-		AdministrativeAreaLevel1 = 6,
+        /// <summary>
+        /// Indicates a first-order civil entity below the country level. Within the
+        /// United States, these administrative levels are states. Not all nations
+        /// exhibit these administrative levels.
+        /// </summary>
+        AdministrativeAreaLevel1 = 6,
 
-		/// <summary>
-		/// Indicates a second-order civil entity below the country level. Within the
-		/// United States, these administrative levels are counties. Not all nations
-		/// exhibit these administrative levels.
-		/// </summary>
-		AdministrativeAreaLevel2 = 7,
+        /// <summary>
+        /// Indicates a second-order civil entity below the country level. Within the
+        /// United States, these administrative levels are counties. Not all nations
+        /// exhibit these administrative levels.
+        /// </summary>
+        AdministrativeAreaLevel2 = 7,
 
-		/// <summary>
-		/// Indicates a third-order civil entity below the country level. This type
+        /// <summary>
+        /// Indicates a third-order civil entity below the country level. This type
+        /// indicates a minor civil division. Not all nations exhibit these
+        /// administrative levels.
+        /// </summary>
+        AdministrativeAreaLevel3 = 8,
+
+        /// <summary>
+		/// Indicates a fourth-order civil entity below the country level. This type
 		/// indicates a minor civil division. Not all nations exhibit these
 		/// administrative levels.
 		/// </summary>
-		AdministrativeAreaLevel3 = 8,
+		AdministrativeAreaLevel4 = 31,
 
-		/// <summary>
-		/// Indicates a commonly-used alternative name for the entity.
+        /// <summary>
+		/// Indicates a fifth-order civil entity below the country level. This type
+		/// indicates a minor civil division. Not all nations exhibit these
+		/// administrative levels.
 		/// </summary>
-		ColloquialArea = 9,
+		AdministrativeAreaLevel5 = 32,
 
-		/// <summary>
-		/// Indicates an incorporated city or town political entity.
-		/// </summary>
-		Locality = 10,
+        /// <summary>
+        /// Indicates a commonly-used alternative name for the entity.
+        /// </summary>
+        ColloquialArea = 9,
 
-		/// <summary>
+        /// <summary>
+        /// Indicates an incorporated city or town political entity.
+        /// </summary>
+        Locality = 10,
+
+        /// <summary>
+        /// Indicates an first-order civil entity below a locality.
+        /// </summary>
+        Sublocality = 11,
+
+        /// <summary>
 		/// Indicates an first-order civil entity below a locality.
 		/// </summary>
-		Sublocality = 11,
+        SublocalityLevel1 = 26,
 
-		/// <summary>
-		/// Indicates a named neighborhood.
+        /// <summary>
+		/// Indicates an second-order civil entity below a locality.
 		/// </summary>
-		Neighborhood = 12,
+        SublocalityLevel2 = 27,
 
-		/// <summary>
-		/// Indicates a named location, usually a building or collection of
-		/// buildings with a common name.
+        /// <summary>
+		/// Indicates an third-order civil entity below a locality.
 		/// </summary>
-		Premise = 13,
+        SublocalityLevel3 = 28,
 
-		/// <summary>
-		/// Indicates a first-order entity below a named location, usually a
-		/// singular building within a collection of buildings with a common
-		/// name.
+        /// <summary>
+		/// Indicates an fourth-order civil entity below a locality.
 		/// </summary>
-		Subpremise = 14,
+        SublocalityLevel4 = 29,
 
-		/// <summary>
-		/// Indicates a postal code as used to address postal mail within the
-		/// country.
+        /// <summary>
+		/// Indicates an fifth-order civil entity below a locality.
 		/// </summary>
-		PostalCode = 15,
+        SublocalityLevel5 = 30,
 
-		/// <summary>
-		/// Indicates a prominent natural feature.
-		/// </summary>
-		NaturalFeature = 16,
+        /// <summary>
+        /// Indicates a named neighborhood.
+        /// </summary>
+        Neighborhood = 12,
 
-		/// <summary>
-		/// Indicates an airport.
-		/// </summary>
-		Airport = 17,
+        /// <summary>
+        /// Indicates a named location, usually a building or collection of
+        /// buildings with a common name.
+        /// </summary>
+        Premise = 13,
 
-		/// <summary>
-		/// Indicates a named park.
-		/// </summary>
-		Park = 18,
+        /// <summary>
+        /// Indicates a first-order entity below a named location, usually a
+        /// singular building within a collection of buildings with a common
+        /// name.
+        /// </summary>
+        Subpremise = 14,
 
-		/// <summary>
-		/// Indicates a named point of interest. Typically, these "POI"s are
-		/// prominent local entities that don't easily fit in another category
-		/// such as "Empire State Building" or "Statue of Liberty."
-		/// </summary>
-		PointOfInterest = 19,
+        /// <summary>
+        /// Indicates a postal code as used to address postal mail within the
+        /// country.
+        /// </summary>
+        PostalCode = 15,
 
-		/// <summary>
-		/// Indicates a specific postal box.
-		/// </summary>
-		PostBox = 20,
+        /// <summary>
+        /// Indicates a prominent natural feature.
+        /// </summary>
+        NaturalFeature = 16,
 
-		/// <summary>
-		/// Indicates the precise street number.
-		/// </summary>
-		StreetNumber = 21,
+        /// <summary>
+        /// Indicates an airport.
+        /// </summary>
+        Airport = 17,
 
-		/// <summary>
-		/// Indicates the floor of a building address.
-		/// </summary>
-		Floor = 22,
+        /// <summary>
+        /// Indicates a named park.
+        /// </summary>
+        Park = 18,
 
-		/// <summary>
-		/// Indicates the room of a building address.
-		/// </summary>
-		Room = 23,
+        /// <summary>
+        /// Indicates a named point of interest. Typically, these "POI"s are
+        /// prominent local entities that don't easily fit in another category
+        /// such as "Empire State Building" or "Statue of Liberty."
+        /// </summary>
+        PointOfInterest = 19,
 
-		/// <summary>
-		/// (Not documented) Indicates the Postal Town (nearest large town / city for UK addresses)
-		/// </summary>
-		PostalTown = 24,
+        /// <summary>
+        /// Indicates a specific postal box.
+        /// </summary>
+        PostBox = 20,
 
-		/// <summary>
-		/// (Not documented) First half of postcode for the UK
-		/// </summary>
-		PostalCodePrefix = 25
-	}
+        /// <summary>
+        /// Indicates the precise street number.
+        /// </summary>
+        StreetNumber = 21,
+
+        /// <summary>
+        /// Indicates the floor of a building address.
+        /// </summary>
+        Floor = 22,
+
+        /// <summary>
+        /// Indicates the room of a building address.
+        /// </summary>
+        Room = 23,
+
+        /// <summary>
+        /// (Not documented) Indicates the Postal Town (nearest large town / city for UK addresses)
+        /// </summary>
+        PostalTown = 24,
+
+        /// <summary>
+        /// (Not documented) First half of postcode for the UK
+        /// </summary>
+        PostalCodePrefix = 25
+    }
 }
