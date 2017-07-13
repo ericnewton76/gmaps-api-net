69d9dfc6cb77a91988a06bb4540ac57332599273
Richard Thombs <richard@teamhaven.com>
2017-07-13 15:57:07 +0100

    feat: Radar search is deprecated as of June 30, 2018
    See: https://developers.google.com/places/web-service/search#RadarSearchRequests

--- src/Google.Maps/Places/RadarSearchRequest.cs
+++ src/Google.Maps/Places/RadarSearchRequest.cs
@@ -27,6 +27,7 @@ namespace Google.Maps.Places
 	/// Search, you can create applications that help users identify specific
 	/// areas of interest within a geographic area
 	/// </summary>
+	[Obsolete("Radar search is deprecated as of June 30, 2018. After that time, this feature will no longer be available.")]
 	public class RadarSearchRequest : PlacesRequest
 	{
 		/// <summary>
