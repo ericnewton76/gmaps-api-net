63bd5c8806406ba7f3d020365075627a435b7c86
Richard Thombs <richard@teamhaven.com>
2017-07-18 23:08:22 +0100

    chore(Tests): test GeocodingService against the live service

--- src/Google.Maps.Test/Geocoding/GeocodingServiceTests.cs
+++ src/Google.Maps.Test/Geocoding/GeocodingServiceTests.cs
@@ -26,245 +26,60 @@ namespace Google.Maps.Test.Integrations
 	[TestFixture]
 	class GeocodingServiceTests
 	{
-
-		private static double GetTolerance(double expected, int decimalPrecision)
-		{
-			int magnitude = 1 + (expected == 0.0 ? -1 : Convert.ToInt32(Math.Floor(Math.Log10(expected))));
-			int precision = 15 - magnitude;
-
-			double tolerance = 1.0 / Math.Pow(10, precision);
-
-			return tolerance;
-		}
-
-		private static AddressComponent MakeAddressComponent(string shortName, string longName, params AddressType[] types)
-		{
-			return new AddressComponent()
-			{
-				ShortName = shortName,
-				LongName = longName,
-				Types = types
-			};
-		}
-		private static Geometry MakeGeometry(LocationType locationType, double locationLat, double locationLong, double swLat, double swLong, double neLat, double neLong)
-		{
-			return new Geometry()
-			{
-				LocationType = locationType
-				,
-				Location = new LatLng(locationLat, locationLong)
-				,
-				Viewport = new Viewport(
-				  southWest: new LatLng(swLat, swLong)
-				  , northEast: new LatLng(neLat, neLong)
-					)
-			};
-		}
-
-		#region TestFixtureSetup/TearDown
-		[OneTimeSetUp]
-		public void FixtureSetup()
-		{
-			Google.Maps.Internal.Http.Factory = new Google.Maps.Test.Integrations.HttpGetResponseFromResourceFactory("Google.Maps.Test.Geocoding");
-		}
-
-		[OneTimeTearDown]
-		public void FixtureTearDown()
-		{
-			Google.Maps.Internal.Http.Factory = new Internal.Http.HttpGetResponseFactory();
-		}
-		#endregion
-
 		[Test]
 		public void Empty_address()
 		{
-			// expectations
-			var expectedStatus = ServiceResponseStatus.ZeroResults;
-			var expectedResultCount = 0;
-
-			// test
-			var request = new GeocodingRequest();
-			request.Address = "";
-			var response = new GeocodingService().GetResponse(request);
-
-			// asserts
-			Assert.AreEqual(expectedStatus, response.Status);
-			Assert.AreEqual(expectedResultCount, response.Results.Count());
+			Assert.Throws<System.Net.WebException>(() =>
+			{
+				var request = new GeocodingRequest { Address = "" };
+				var response = new GeocodingService().GetResponse(request);
+			});
 		}
 
 		[Test]
 		public void GetGeocodingForAddress1()
 		{
-			// expectations
-			var expectedStatus = ServiceResponseStatus.Ok;
-			var expectedResultCount = 1;
-			var expectedType = AddressType.StreetAddress;
-			var expectedFormattedAddress = "1600 Amphitheatre Parkway, Mountain View, CA 94043, USA";
-			var expectedComponentTypes = new AddressType[] {
-				AddressType.StreetNumber,
-				AddressType.Route,
-				AddressType.Locality,
-				AddressType.AdministrativeAreaLevel1,
-				AddressType.AdministrativeAreaLevel2,
-				AddressType.AdministrativeAreaLevel3,
-				AddressType.Country,
-				AddressType.PostalCode,
-				AddressType.Political
-			};
+			// Arrange
+			var request = new GeocodingRequest { Address = "1600 Amphitheatre Parkway Mountain View CA" };
+
+			// Act
+			var response = new GeocodingService().GetResponse(request);
+
+			// Assert
+			Assert.AreEqual(ServiceResponseStatus.Ok, response.Status);
+			Assert.AreEqual(1, response.Results.Length);
+
+			var result = response.Results.Single();
+			Assert.AreEqual("Google Bldg 41, 1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA", result.FormattedAddress);
+			Assert.AreEqual(LocationType.Rooftop, result.Geometry.LocationType);
+
 			var expectedLocation = new LatLng(37.42219410, -122.08459320);
-			var expectedLocationType = LocationType.Rooftop;
+			Assert.That(expectedLocation, Is.EqualTo(result.Geometry.Location).Using(LatLngComparer.Within(0.00001f)));
+
 			Viewport expectedViewport = new Viewport(
 				southWest: new LatLng(37.42084511970850, -122.0859421802915),
 				northEast: new LatLng(37.42354308029149, -122.0832442197085)
 			);
-
-			// test
-			var request = new GeocodingRequest();
-			request.Address = "1600 Amphitheatre Parkway Mountain View CA";
-			var response = new GeocodingService().GetResponse(request);
-
-			// asserts
-			Assert.AreEqual(expectedStatus, response.Status, "Status");
-			Assert.AreEqual(expectedResultCount, response.Results.Length, "ResultCount");
-			Assert.AreEqual(expectedType, response.Results.Single().Types.First(), "Type");
-			Assert.AreEqual(expectedFormattedAddress, response.Results.Single().FormattedAddress, "FormattedAddress");
-			//Assert.IsTrue(
-			//    expectedComponentTypes.OrderBy(x => x).SequenceEqual(
-			//        response.Results.Single().AddressComponents.SelectMany(y => y.Types).Distinct().OrderBy(z => z)), "Types");
-			//Assert.AreEqual(expectedLatitude, response.Results.Single().Geometry.Location.Latitude, "Latitude");
-			Assert.That(expectedLocation, Is.EqualTo(response.Results[0].Geometry.Location).Using(LatLngComparer.Within(0.000001f)), "Longitude");
-			Assert.AreEqual(expectedLocationType, response.Results.Single().Geometry.LocationType, "LocationType");
-			//Assert.AreEqual(expectedSouthwestLatitude, response.Results.Single().Geometry.Viewport.Southwest.Latitude, "Southwest.Latitude");
-			//Assert.AreEqual(expectedSouthwestLongitude, response.Results.Single().Geometry.Viewport.Southwest.Longitude, "Southwest.Longitude");
-			//Assert.AreEqual(expectedNortheastLatitude, response.Results.Single().Geometry.Viewport.Northeast.Latitude, "Northeast.Latitude");
-			//Assert.AreEqual(expectedNortheastLongitude, response.Results.Single().Geometry.Viewport.Northeast.Longitude, "Northeast.Longitude");
+			Assert.That(expectedViewport.Southwest, Is.EqualTo(result.Geometry.Viewport.Southwest).Using(LatLngComparer.Within(0.00001f)));
+			Assert.That(expectedViewport.Northeast, Is.EqualTo(result.Geometry.Viewport.Northeast).Using(LatLngComparer.Within(0.00001f)));
 		}
 
 
 		[Test]
 		public void GetGeocodingForAddress2()
 		{
-			// expectations
-			GeocodeResponse expected = new GeocodeResponse()
-			{
-				Status = ServiceResponseStatus.Ok,
-				Results = new Result[] {
-					new Result() {
-
-						AddressComponents = new AddressComponent[] {
-							MakeAddressComponent("11","11",	AddressType.StreetNumber)
-							, MakeAddressComponent("Wall St","Wall Street", AddressType.Route)
-							, MakeAddressComponent("Lower Manhattan", "Lower Manhattan", AddressType.Neighborhood, AddressType.Political)
-							, MakeAddressComponent("Manhattan", "Manhattan", AddressType.Sublocality, AddressType.Political)
-							, MakeAddressComponent("New York", "New York", AddressType.Locality, AddressType.Political)
-							, MakeAddressComponent("New York", "New York", AddressType.AdministrativeAreaLevel2, AddressType.Political)
-							, MakeAddressComponent("NY", "New York", AddressType.AdministrativeAreaLevel1, AddressType.Political)
-							, MakeAddressComponent("US", "United States", AddressType.Country, AddressType.Political)
-						}
-						, FormattedAddress = "11 Wall Street, New York, NY 10005, USA"
-						, Geometry = MakeGeometry(LocationType.Rooftop,
-								40.7068599,-74.0111281 //location
-								, 40.7055109,-74.0124771 //swBound
-								, 40.7082089,-74.0097791) //neBound
-						, Types = new AddressType[] { AddressType.StreetAddress }
-					}
-				}
-			};
-
-
 			// test
 			var request = new GeocodingRequest();
 			request.Address = "11 Wall Street New York NY 10005";
 			var actual = new GeocodingService().GetResponse(request);
 
 			// asserts
-			Assert.AreEqual(expected.Status, actual.Status, "Status");
-			Assert.AreEqual(expected.Results.Length, actual.Results.Length, "ResultCount");
+			Assert.AreEqual(ServiceResponseStatus.Ok, actual.Status);
+			Assert.AreEqual(1, actual.Results.Length);
 
-			var expectedResult = expected.Results.First(); var actualResult = actual.Results.First();
-			Assert.AreEqual(expectedResult.Types, actualResult.Types, "Result.First().Types");
-			Assert.AreEqual(expectedResult.FormattedAddress, actualResult.FormattedAddress, "Resut.First().FormattedAddress");
-
-			//Assert.That(expectedResult.AddressComponents, Is.EquivalentTo(actualResult.AddressComponents));
-
-			//Assert.IsTrue(
-			//    expectedComponentTypes.OrderBy(x => x).SequenceEqual(
-			//        response.Results.Single().AddressComponents.SelectMany(y => y.Types).Distinct().OrderBy(z => z)), "Types");
-
-			//tolerance needed when testing doubles
-			//http://stackoverflow.com/questions/4787125/evaluate-if-two-doubles-are-equal-based-on-a-given-precision-not-within-a-certa
-			//double tolerance = GetTolerance(expectedResult.Geometry.Viewport.Southwest.Latitude, 7);
-			//Assert.That(expectedResult.Geometry.Viewport.Southwest, Is.EqualTo(actualResult.Geometry.Viewport.Southwest).Within(latlngTolerance));
-			//Assert.That(expectedResult.Geometry.Viewport.Southwest, Is.EqualTo(actualResult.Geometry.Viewport.Southwest).Within(0.0000001d));
-
-			//Assert.AreEqual(expectedLatitude, response.Results.Single().Geometry.Location.Latitude, "Latitude");
-			//Assert.AreEqual(expectedLongitude, response.Results.Single().Geometry.Location.Longitude, "Longitude");
-			//Assert.AreEqual(expectedLocationType, response.Results.Single().Geometry.LocationType, "LocationType");
-			//Assert.AreEqual(expectedSouthwestLatitude, response.Results.Single().Geometry.Viewport.Southwest.Latitude, "Southwest.Latitude");
-			//Assert.AreEqual(expectedSouthwestLongitude, response.Results.Single().Geometry.Viewport.Southwest.Longitude, "Southwest.Longitude");
-			//Assert.AreEqual(expectedNortheastLatitude, response.Results.Single().Geometry.Viewport.Northeast.Latitude, "Northeast.Latitude");
-			//Assert.AreEqual(expectedNortheastLongitude, response.Results.Single().Geometry.Viewport.Northeast.Longitude, "Northeast.Longitude");
+			var actualResult = actual.Results.Single();
+			Assert.AreEqual(new AddressType[] { AddressType.StreetAddress }, actualResult.Types);
+			Assert.AreEqual("11 Wall St, New York, NY 10005, USA", actualResult.FormattedAddress);
 		}
-
-		//[Test]
-		//public void GetGeocodingForCoordinates()
-		//{
-		//    // expectations
-		//    var expectedStatus = ServiceResponseStatus.Ok;
-		//    var expectedResultCount = 9;
-		//    var expectedTypes = new AddressType[] {
-		//        AddressType.StreetAddress,
-		//        AddressType.Locality,
-		//        AddressType.PostalCode,
-		//        AddressType.Sublocality,
-		//        AddressType.AdministrativeAreaLevel2,
-		//        AddressType.AdministrativeAreaLevel1,
-		//        AddressType.Country,
-		//        AddressType.Political
-		//    };
-		//    var expectedFormattedAddress = "277 Bedford Ave, Brooklyn, NY 11211, USA";
-		//    var expectedComponentTypes = new AddressType[] {
-		//        AddressType.StreetNumber,
-		//        AddressType.Route,
-		//        AddressType.Locality,
-		//        AddressType.AdministrativeAreaLevel1,
-		//        AddressType.AdministrativeAreaLevel2,
-		//        AddressType.Sublocality,
-		//        AddressType.Country,
-		//        AddressType.PostalCode,
-		//        AddressType.Political
-		//    };
-		//    double expectedLatitude = 40.7142330;
-		//    double expectedLongitude = -73.9612910;
-		//    LocationType expectedLocationType = LocationType.Rooftop;
-		//    double expectedSouthwestLatitude = 40.7110854;
-		//    double expectedSouthwestLongitude = -73.9644386;
-		//    double expectedNortheastLatitude = 40.7173806;
-		//    double expectedNortheastLongitude = -73.9581434;
-
-		//    // test
-		//    var request = new GeocodingRequest();
-		//    request.Address = new LatLng(expectedLatitude, expectedLongitude);
-		//    request.Sensor = false;
-		//    var response = GeocodingService.GetResponse(request);
-
-		//    // asserts
-		//    Assert.AreEqual(expectedStatus, response.Status, "Status");
-		//    Assert.AreEqual(expectedResultCount, response.Results.Length, "ResultCount");
-		//    Assert.IsTrue(
-		//        expectedTypes.OrderBy(x => x).SequenceEqual(
-		//            response.Results.SelectMany(y => y.Types).Distinct().OrderBy(z => z)));
-		//    Assert.AreEqual(expectedFormattedAddress, response.Results.First().FormattedAddress, "FormattedAddress");
-		//    //Assert.IsTrue(
-		//    //    expectedComponentTypes.OrderBy(x => x).SequenceEqual(
-		//    //        response.Results.First().AddressComponents.SelectMany(y => y.Types).Distinct().OrderBy(z => z)), "Types");
-		//    Assert.AreEqual(expectedLatitude, response.Results.First().Geometry.Location.Latitude, "Latitude");
-		//    Assert.AreEqual(expectedLongitude, response.Results.First().Geometry.Location.Longitude, "Longitude");
-		//    Assert.AreEqual(expectedLocationType, response.Results.First().Geometry.LocationType, "LocationType");
-		//    Assert.AreEqual(expectedSouthwestLatitude, response.Results.First().Geometry.Viewport.Southwest.Latitude, "Southwest.Latitude");
-		//    Assert.AreEqual(expectedSouthwestLongitude, response.Results.First().Geometry.Viewport.Southwest.Longitude, "Southwest.Longitude");
-		//    Assert.AreEqual(expectedNortheastLatitude, response.Results.First().Geometry.Viewport.Northeast.Latitude, "Northeast.Latitude");
-		//    Assert.AreEqual(expectedNortheastLongitude, response.Results.First().Geometry.Viewport.Northeast.Longitude, "Northeast.Longitude");
-		//}
 	}
 }
