15506a565bebeb3baeec4f3bfc42a4164c1a6306
Eric Newton <ericnewton76@gmail.com>
2017-07-20 18:21:50 -0400

    during testing, get api key from the GOOGLE_API_KEY environment variable

--- src/Google.Maps.Test/SigningHelper.cs
+++ src/Google.Maps.Test/SigningHelper.cs
@@ -8,6 +8,35 @@ namespace Google.Maps
 {
 	public static class SigningHelper
 	{
+		static SigningHelper()
+		{
+			//during testing, get api key from the GOOGLE_API_KEY environment variable, to enable more flexibility and try to prevent OverQuotaLimit 
+			S_ApiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
+
+			//Log.Info("GetEnvironmentVariable(\"GOOGLE_API_KEY\")='{0}'.",S_ApiKey);
+
+			//for local testing, you can either use the Debugger to modify the S_ApiKey static variable
+			//or set the Debugging Environment variable via the Visual Stdio Debugging properties dialog. 
+			//for more info, see https://stackoverflow.com/a/155363/323456
+
+			//for nunit test, might need to use the NUnit Test Settings file
+
+			//if env["GOOGLE_API_KEY"] is empty, use a default key
+			if(string.IsNullOrEmpty(S_ApiKey))
+			{
+				//Log.Info("Setting ApiKey to a default key.");
+
+				//this api key can be used for individual developer machines
+				//you may get OverQueryLimit responses though
+				S_ApiKey = "AIzaSyDV-0ftj1tsjfd6GnEbtbxwHXnv6iR3UEU";
+			}
+
+			//Log.Info("Using ApiKey '{0}' for testing.", S_ApiKey);
+		}
+
+		/// <summary>Holds the ApiKey used for testing</summary>
+		private static string S_ApiKey;
+
 		public static GoogleSigned GetPrivateKey()
 		{
 			throw new NotImplementedException();
@@ -15,7 +44,7 @@ namespace Google.Maps
 
 		public static GoogleSigned GetApiKey()
 		{
-			return new GoogleSigned("AIzaSyDV-0ftj1tsjfd6GnEbtbxwHXnv6iR3UEU");
+			return new GoogleSigned(S_ApiKey);
 		}
 	}
 }
