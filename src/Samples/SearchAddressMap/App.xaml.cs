61638ba3afbe5045d3e651813f7a4c8ecd7895c6
Eric Newton <ericnewton76@gmail.com>
2017-10-21 19:54:26 -0400

    implemented StreetView API in the sample app for image/url implementation testing

--- src/Samples/SearchAddressMap/App.xaml.cs
+++ src/Samples/SearchAddressMap/App.xaml.cs
@@ -8,5 +8,11 @@ namespace SearchAddressMap
 	/// </summary>
 	public partial class App : Application
 	{
+		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			string errorMessage = string.Format("An unhandled exception occurred: {0}", e.Exception.Message);
+			MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			e.Handled = true;
+		}
 	}
 }
