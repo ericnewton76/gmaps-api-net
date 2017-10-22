f1a5cb5d84592249ee47891fd9a15fa5637c4aeb
Eric Newton <ericnewton76@gmail.com>
2017-10-21 23:55:28 -0400

    added fluentassertions to add better collection testing for querystring testing during uri building

--- src/Google.Maps.Test/StreetView/StreetView_uribuilding_Tests.cs
+++ src/Google.Maps.Test/StreetView/StreetView_uribuilding_Tests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 using NUnit.Framework;
 using Google.Maps.StreetView;
+using FluentAssertions.Collections;
+using FluentAssertions;
 
 namespace Google.Maps.Tests.StreetView
 {
@@ -11,27 +13,54 @@ namespace Google.Maps.Tests.StreetView
 	{
 		Uri gmapsBaseUri = new Uri("http://maps.google.com/");
 
+		private Dictionary<string,string> ParseQueryString(string querystring)
+		{
+			Dictionary<string, string> values = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+			int indexOfQmark = querystring.IndexOf("?");
+
+			if(indexOfQmark>-1)
+			{
+				querystring = querystring.Substring(indexOfQmark + 1);
+			}
+
+			string[] kvpairs = querystring.Split('&');
+			foreach(var keyequalvalue in kvpairs)
+			{
+				string[] kv = keyequalvalue.Split('=');
+				values[kv[0]] = kv[1];
+
+			}
+
+			return values;
+		}
+
+
 		[Test]
 		public void BasicUri()
 		{
-			string expected = "/maps/api/streetview?location=30.1,-60.2&size=512x512";
-
+			//arrange
+			var expected = ParseQueryString("/maps/api/streetview?location=30.1,-60.2&size=512x512");
+			
 			StreetViewRequest sm = new StreetViewRequest()
 			{
 				Location = new LatLng(30.1, -60.2)
 				,Size = new MapSize(512, 512)
 			};
 
+			//act
 			Uri actualUri = sm.ToUri();
-			string actual = actualUri.PathAndQuery;
+			var actual = ParseQueryString(actualUri.PathAndQuery);
 
-			Assert.AreEqual(expected, actual);
+			//assert
+			actual.ShouldAllBeEquivalentTo(expected);
 		}
 
 		[Test]
 		public void BasicUri_heading()
 		{
-			string expected = "/maps/api/streetview?location=30.1,-60.2&size=512x512&heading=15";
+			//arrange
+			var expected = ParseQueryString("/maps/api/streetview?location=30.1,-60.2&size=512x512&heading=15");
 
 			StreetViewRequest sm = new StreetViewRequest()
 			{
@@ -40,16 +69,19 @@ namespace Google.Maps.Tests.StreetView
 				,Heading = 15
 			};
 
+			//act
 			Uri actualUri = sm.ToUri();
-			string actual = actualUri.PathAndQuery;
+			var actual = ParseQueryString(actualUri.PathAndQuery);
 
-			Assert.AreEqual(expected, actual);
+			//assert
+			actual.ShouldAllBeEquivalentTo(expected);
 		}
 
 		[Test]
 		public void BasicUri_pitch()
 		{
-			string expected = "/maps/api/streetview?location=30.1,-60.2&size=512x512&pitch=15";
+			//arrange
+			var expected = ParseQueryString("/maps/api/streetview?location=30.1,-60.2&size=512x512&pitch=15");
 
 			StreetViewRequest sm = new StreetViewRequest()
 			{
@@ -58,10 +90,13 @@ namespace Google.Maps.Tests.StreetView
 				,Pitch = 15
 			};
 
+			//act
 			Uri actualUri = sm.ToUri();
-			string actual = actualUri.PathAndQuery;
+			var actual = ParseQueryString(actualUri.PathAndQuery);
 
-			Assert.AreEqual(expected, actual);
+			//assert
+			actual.ShouldAllBeEquivalentTo(expected);
 		}
+
 	}
 }
