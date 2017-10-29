e7b55d34eca11a36d2e35dd46db9b0653d2096c2
Eric Newton <ericnewton76@gmail.com>
2017-10-29 13:02:12 -0400

    removed sensor property

--- src/Google.Maps/BaseRequest.cs
+++ src/Google.Maps/BaseRequest.cs
@@ -7,9 +7,6 @@ namespace Google.Maps
 {
 	public abstract class BaseRequest
 	{
-		[Obsolete("The Google Maps API no longer requires this parameter and it will be removed in the next release")]
-		public bool? Sensor { get; set; }
-
 		public abstract Uri ToUri();
 	}
 }
