1146df480fb93b981a8248594771c4bc210acf7e
Eric Newton <ericnewton76@gmail.com>
2017-10-29 20:14:15 -0400

    created default facade for easiest access to the services without using DI container

---
+++ src/Google.Maps/_Services.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Google.ApiCore;
+
+namespace Google.Maps
+{
+
+	/// <summary>
+	/// Easy to use facade for all the services
+	/// </summary>
+    public class Services
+    {
+		public Services(string apiKey)
+		{
+			this._SigningService = new GoogleApiSigningService(new GoogleSigned(apiKey));
+			this._HttpService = new Google.Maps.Internal.MapsHttp(this._SigningService);
+		}
+
+		public Services WithSigningService(ISigningService signingService)
+		{
+			this._SigningService = signingService;
+			return this;
+		}
+		public Services WithHttpService(IHttpService httpService)
+		{
+			this._HttpService = httpService;
+			return this;
+		}
+
+		private GoogleSigned _signingInstance;
+		private ISigningService _SigningService;
+		private IHttpService _HttpService;
+
+		public ISigningService SigningService { get { return this._SigningService; } }
+		public IHttpService HttpService { get { return this._HttpService; } }
+
+	}
+}
